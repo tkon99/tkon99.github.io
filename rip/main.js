0a5cdee6c71b838c529e8fd5128438b7e768b1f7
tkon99 <tkon99@gmail.com>
2015-03-20 16:49:38 +0100

    Even more RIP'

--- rip/main.js
+++ rip/main.js
@@ -2,22 +2,42 @@ var state = false;
 //true = color: white
 //false = color: black
 
-function colors(){
-	if(state == false){
-		document.getElementById("text").style.color = "black"; 
-		document.getElementById("back").style.backgroundColor = "white";
-	}else{
-		document.getElementById("text").style.color = "white"; 
-		document.getElementById("back").style.backgroundColor = "black";
-	}
+var stop = false;
 
-	state = !state;
+function colors(){
+	if(stop !== true){
+		if(state == false){
+			document.getElementById("text").style.color = "black"; 
+			document.getElementById("hash").style.color = "black";
+			document.getElementById("back").style.backgroundColor = "white";
+		}else{
+			document.getElementById("text").style.color = "white"; 
+			document.getElementById("hash").style.color = "white";
+			document.getElementById("back").style.backgroundColor = "black";
+		}
+
+		state = !state;
+	}
 
 	setTimeout(function(){
 		return colors();
 	}, 50);
 }
 
+function colorSwitch(){
+	if(stop == false){
+		stop = true;
+	}else{
+		stop = false;
+	}
+}
+
 $(document).ready(function(){
 	colors();
+
+	var hash = location.hash.substring(1);
+	if(hash !== "" && hash !== undefined){
+		document.title = "RIP. "+hash;
+		$("#hash").html(hash);
+	}
 });
