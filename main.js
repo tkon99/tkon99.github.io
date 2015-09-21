0aa83dd701e703bb121d14bb670eeda38afefc9c
tkon99 <tkon99@gmail.com>
2015-09-21 19:31:18 +0200

    fixed last.fm

--- main.js
+++ main.js
@@ -33,11 +33,11 @@ $(document).ready(function(){
 	});
 
 	//LastFM
-	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%20%3D%20%22http://ws.audioscrobbler.com/1.0/user/tkon99/recenttracks.rss%22%20and%20xpath%3D%22*%22&format=json";
+	var url = "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=tkon99&api_key=5b801a66d1a34e73b6e563afc27ef06b&limit=1&format=json";
 	$.getJSON(url, function(data){
-		//console.log(data);
-		var desc = data.query.results.html.body.rss.channel.item[0].content;
-		$("#song").html(removelinks(desc));
+		var track = data["recenttracks"]["track"][0];
+		var desc = '<a href="'+track["url"]+'" target="_blank">'+track["name"]+' by '+track["artist"]["#text"]+'</a>';
+		$("#song").html(desc);
 	});
 
 	var hash = window.location.hash.substring(1);
