70d262205b177bf502502a55017670193e9c3b0c
tkon99 <tkon99@gmail.com>
2016-02-06 17:39:06 +0100

    new version

--- main.js
+++ main.js
@@ -27,19 +27,15 @@ function pad(value) {
         return value;
     }
 }
+!function(a){var b=/iPhone/i,c=/iPod/i,d=/iPad/i,e=/(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,f=/Android/i,g=/IEMobile/i,h=/(?=.*\bWindows\b)(?=.*\bARM\b)/i,i=/BlackBerry/i,j=/BB10/i,k=/Opera Mini/i,l=/(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,m=new RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)","i"),n=function(a,b){return a.test(b)},o=function(a){var o=a||navigator.userAgent,p=o.split("[FBAN");return"undefined"!=typeof p[1]&&(o=p[0]),this.apple={phone:n(b,o),ipod:n(c,o),tablet:!n(b,o)&&n(d,o),device:n(b,o)||n(c,o)||n(d,o)},this.android={phone:n(e,o),tablet:!n(e,o)&&n(f,o),device:n(e,o)||n(f,o)},this.windows={phone:n(g,o),tablet:n(h,o),device:n(g,o)||n(h,o)},this.other={blackberry:n(i,o),blackberry10:n(j,o),opera:n(k,o),firefox:n(l,o),device:n(i,o)||n(j,o)||n(k,o)||n(l,o)},this.seven_inch=n(m,o),this.any=this.apple.device||this.android.device||this.windows.device||this.other.device||this.seven_inch,this.phone=this.apple.phone||this.android.phone||this.windows.phone,this.tablet=this.apple.tablet||this.android.tablet||this.windows.tablet,"undefined"==typeof window?this:void 0},p=function(){var a=new o;return a.Class=o,a};"undefined"!=typeof module&&module.exports&&"undefined"==typeof window?module.exports=o:"undefined"!=typeof module&&module.exports&&"undefined"!=typeof window?module.exports=p():"function"==typeof define&&define.amd?define("isMobile",[],a.isMobile=p()):a.isMobile=p()}(this);
 
-$(document).ready(function(){
-	$.modal.defaults = {
-	  opacity: 0.1,          // Overlay opacity
-	  zIndex: 1,              // Overlay z-index.
-	  escapeClose: true,      // Allows the user to close the modal by pressing `ESC`
-	  clickClose: true,       // Allows the user to close the modal by clicking the overlay
-	  modalClass: "modal",    // CSS class added to the element being displayed in the modal.
-	};
-
-	$(document).on($.modal.CLOSE, function(event, modal) {
-		window.location.hash = "";
-	});
+$(document).ready(function(){	
+	if(isMobile.any){
+		$("body").removeClass("desktop");
+		$("body").addClass("mobile");
+		$(".fix-height").height($(window).height() + 60);
+		$(".full-page").attr("style","min-height:"+($(window).height() + 60)+" !important;");
+	}
 
 	//LastFM
 	var url = "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=tkon99&api_key=5b801a66d1a34e73b6e563afc27ef06b&limit=1&format=json";
